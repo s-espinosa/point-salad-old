653d15c01a28266cc6221111053047ac3a09ccdb
Sal Espinosa <sespinos@gmail.com>
2023-12-12 22:30:58 -0600

    Temporarily refactor to top level component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { VeggieComponent } from './veggie/veggie.component';
 import { VeggiesComponent } from './veggies/veggies.component';
+import { Veggie } from './veggie';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,45 @@ import { VeggiesComponent } from './veggies/veggies.component';
 })
 export class AppComponent {
   title = 'point-salad';
+  veggies: Veggie[] = [
+    {
+      name: 'lettuce'
+    },
+    {
+      name: 'onion'
+    },
+    {
+      name: 'cabbage'
+    },
+    {
+      name: 'yellow pepper'
+    },
+    {
+      name: 'carrot'
+    },
+  ]
+  collectedVeggies: Veggie[] = [];
+  veggieTypes = [
+      'lettuce',
+      'onion',
+      'cabbage',
+      'yellow pepper',
+      'carrot',
+  ]
+
+  selectVeggie(veggie: Veggie) {
+    let index = this.veggies.indexOf(veggie)
+    this.veggies.splice(index, 1)
+    this.collectedVeggies.push(veggie)
+  }
+
+  countCollected(veggieName: string) {
+    let count = 0;
+    this.collectedVeggies.forEach((veggie) => {
+      if(veggie.name === veggieName) {
+        count++
+      }
+    })
+    return count
+  }
 }
